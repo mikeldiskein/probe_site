089be9376930e73b42019c95ee714e985e21ac88
mikeldiskein <101710767+mikeldiskein@users.noreply.github.com>
2023-02-14 18:47:43 +0500

    initial_1

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-bq7%l3x1f3u65zq_20-_49_(z(34zoobjwp6*@q7i@k6fd-owl'
-import os
+
 SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY',
     'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
